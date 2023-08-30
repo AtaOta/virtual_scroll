c90c36096a8ee680f822b97481b5bb539ffdb255
unknown <ataota.org@gmail.com>
2023-08-30 14:21:17 +0530

    VERSION-1.0.8, virtual scroll modified

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/virtual_scroll/',  // COMMENT-OUT DURING DEVELOPMENT
+  base: '/virtual_scroll/',  // COMMENT-OUT DURING DEVELOPMENT
   plugins: [vue()],
-  // resolve: { // COMMENT-OUT DURING DEVELOPMENT
-  //   alias: {
-  //     '@': fileURLToPath(new URL('./src', import.meta.url))
-  //   }
-  // }
+  resolve: { // COMMENT-OUT DURING DEVELOPMENT
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
