a094d6fdce68a9c6f17e792f51ee23115c41a9f9
unknown <ataota.org@gmail.com>
2023-08-31 11:50:45 +0530

    VERSION-1.0.9, Infinite Scroll Created

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,20 @@
 import virtualScroll from './components/API/VirtualScroll/virtualScroll.vue';
 import RandomCards from './components/Cards/RandomCards.vue';
 import { ref, onMounted } from 'vue';
+let totalItems = ref(20)
 
 let cards = ref([]);
 
+const BottomTriggerPoint = () =>{
+  for(let i=totalItems.value; i<totalItems.value+20; i++){
+    cards.value.push(i+1)
+  }
+  totalItems.value += 20;
+}
+
 onMounted(()=>{
-  for(let i=0; i<20; i++){
-    cards.value.push(i)
+  for(let i=0; i<totalItems.value; i++){
+    cards.value.push(i+1)
   }
 })
 </script>
@@ -17,7 +25,9 @@ onMounted(()=>{
     Virtual Scroll Component
   </header>
   <section>
-    <virtualScroll>
+    <virtualScroll
+      @callBottomTrigerPoint="BottomTriggerPoint()"
+    >
       <RandomCards :cardObj="cards"/>
     </virtualScroll>
   </section>
--- src/components/API/VirtualScroll/virtualScroll.vue
+++ src/components/API/VirtualScroll/virtualScroll.vue
@@ -3,6 +3,9 @@ import { ref, watch } from 'vue';
 const VirtualScroll = ref(null);
 let VSCdimension = ref(null);
 let NewPosition = ref(null);
+let TrigerPoint = ref(null);
+
+let cbt = defineEmits(['callBottomTrigerPoint'])
 
 const ScrollEvent = () =>{
     // VIRTUAL SCROLL COMPONENT
@@ -12,7 +15,21 @@ const ScrollEvent = () =>{
     VSCdimension.value = VSC.getBoundingClientRect();
     VSC.addEventListener('scroll', ()=>{
         NewPosition.value = VSC.children[0].getBoundingClientRect();
-        console.log(VSC.children[0].getBoundingClientRect())
+        // VSCheight => [ SCROLLABLE SECTION HEIGHT OF VIRTUAL SCROLLER ]
+        let VSCheight = NewPosition.value.height;
+
+        /*  
+            VSCdimension.value.height => [ VIRTUAL SCROLL FIXED CONTAINER HEIGHT ]
+            VSC.scrollTop => [ TOP POSITION OF SCROLLER DURING SCROLL ]
+            (VSCheight*80/100) => [ 80% HEIGHT OF VIRTUAL-SCROLLER SCROLLABLE HEIGHT ] 
+        */
+        if((VSCheight*80/100) <= (VSCdimension.value.height + VSC.scrollTop)){
+            TrigerPoint.value='Triger Point'
+            cbt('callBottomTrigerPoint', true);
+        }
+        else{
+            TrigerPoint.value = ''
+        }
     })
 }
 
@@ -28,6 +45,7 @@ watch(VirtualScroll, ()=>{
         <!-- Top: {{ NewPosition }} -->
         <span v-if="NewPosition">Top: {{ NewPosition.top }},</span>
         <span v-if="NewPosition">Bottom: {{ NewPosition.bottom }}</span>
+        <div style="position: fixed; bottom: 50px; background-color: tomato; left: 0;" v-if="TrigerPoint">{{ TrigerPoint }}</div>
         
     </div>
     <div class="VirtualScroll_Wrapper" ref="VirtualScroll">
